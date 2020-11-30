141712cac87ab9f8c28dc2b214ee3d270c6e5679
Mohammed Ammar Kurabi <kurabi@gmail.com>
2020-12-01 00:53:23 +0400

    Structuring the routes and controllers folders

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
-require('dotenv').config()
-require('colors')
+require('dotenv').config();
+require('colors');
+const express = require('express');
+const bootcamps = require('./routes/bootcamps')
 
-const express = require('express') 
+
+
 const app = express();
+app.use('/api/v1/bootcamps',bootcamps)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`.green.bold);
